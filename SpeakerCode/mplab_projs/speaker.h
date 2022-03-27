57e748ab322cad0a4e644d12796aef6dfa739303
JoaoPontes98 <joao.vrpontes@gmail.com>
2022-03-27 15:09:41 -0300

    Modulating speaker code so far. Need help splitting this into c/h files

---
+++ SpeakerCode/mplab_projs/speaker.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   speaker.h
+ * Author: Joaov
+ *
+ * Created on March 27, 2022, 2:41 PM
+ */
+
+//#ifndef SPEAKER_H
+//#define	SPEAKER_H
+//
+//#include <xc.h>
+//
+//
+//
+//void setupSpeaker();
+////void make_note(int noteNum);
+//
+//
+//#endif	/* SPEAKER_H */
+
